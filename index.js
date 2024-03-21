b43a738784592de254bcf2efe1d2c3833e43fa58
Ken Yeung <yeungchunsiu@gmail.com>
2024-03-21 06:42:40 +0000

    fixed field name

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.post("/api/fileanalyse", upload.single('files'), function (req, res) {
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res) {
   const _body = req.body
   const _respBody = {
     name: req.file.filename,
